7acf191f84a877ecf71905746048d367eff1c666
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2020-03-19 09:44:44 +0100

    Initial commit

---
+++ src/main/java/andreas/webshopexercise/WebshopExerciseApplication.java
@@ -0,0 +1,13 @@
+package andreas.webshopexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/andreas/webshopexercise/WebshopExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package andreas.webshopexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
